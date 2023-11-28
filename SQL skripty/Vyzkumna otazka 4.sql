3a84b2e979a85af4fd3c0cfc11ea26226404a9c3
Kateřina Bálint <k.svobodova.8@seznam.cz>
2023-11-28 13:19:50 +0100

    Uploading SQL script to answer the fourth research question

---
+++ SQL skripty/Vyzkumna otazka 4.sql
@@ -0,0 +1,24 @@
+SELECT DISTINCT
+	CASE 
+		WHEN mezirocni_narust_potraviny - mezirocni_narust_mzdy > 10 THEN 'Ano, takový rok existuje'
+		ELSE 'Ne, takový rok neexistuje'
+	END AS 'Existuje rok, ve kterém byl meziroční nárůst cen potravin výrazně vyšší než růst mezd?'
+FROM (
+	SELECT
+		tk.rok AS rok_1,
+		tk1.rok AS rok_2,
+		avg(tk1.prumerna_hodnota / tk.prumerna_hodnota * 100 - 100) AS mezirocni_narust_mzdy,
+		(SELECT
+			avg(tk3.prumerna_hodnota / tk2.prumerna_hodnota * 100 - 100)
+		FROM t_katerina_balint_project_sql_primary_final tk2 
+		JOIN t_katerina_balint_project_sql_primary_final tk3 ON tk2.rok = tk3.rok - 1 
+			AND tk2.nazev_kategorie = tk3.nazev_kategorie 
+		WHERE tk2.typ_kategorie = 'Zboží' 
+			AND tk.rok = tk2.rok 
+		GROUP BY tk2.rok) AS mezirocni_narust_potraviny
+	FROM t_katerina_balint_project_sql_primary_final tk
+	JOIN t_katerina_balint_project_sql_primary_final tk1 ON tk.rok = tk1.rok - 1
+		AND tk.nazev_kategorie = tk1.nazev_kategorie
+	WHERE tk.typ_kategorie = 'Odvětví'
+	GROUP BY tk.rok
+	HAVING mezirocni_narust_potraviny IS NOT NULL) sjednocene_rozdily;
