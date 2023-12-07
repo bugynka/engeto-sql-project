2c44d2730c6cbe1df0709080686ff7177c08a6a6
Kateřina Bálint <k.svobodova.8@seznam.cz>
2023-12-07 11:28:11 +0100

    Modifing the SQL script according to the task, columns with GINI added, years modified to BETWEEN 2000 and 2021

--- SQL skripty/Zdrojova tabulka 2.sql
+++ SQL skripty/Zdrojova tabulka 2.sql
@@ -2,7 +2,10 @@ CREATE TABLE t_katerina_balint_project_SQL_secondary_final AS
 SELECT
 	c.country,
 	e.`year`,
-	e.GDP
+	e.GDP,
+	e.gini,
+	c.population
 FROM countries c
 JOIN economies e ON c.country = e.country
-WHERE c.continent = 'Europe';
+WHERE c.continent = 'Europe'
+	AND e.YEAR BETWEEN 2000 AND 2021;
